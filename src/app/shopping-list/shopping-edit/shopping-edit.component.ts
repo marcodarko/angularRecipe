a26a0a0834eb0d01d863970bd12b0d8878d1a9c7
marco <artofmarco@gmail.com>
2018-01-04 15:32:26 -0800

    form added

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,28 +1,65 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {Ingridient} from '../../shared/ingridient.model';
 import {ShoppingListService} from '../shopping-list.service';
+import {NgForm} from '@angular/forms';
+import {Subscription} from 'rxjs/Subscription';
+import {ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
-	@ViewChild('nameInput') nameInputRef: ElementRef;
-	@ViewChild('amountInput') amountInputRef: ElementRef;
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+
+  @ViewChild('myForm') slForm: NgForm;
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex :number;
+  editedItem: Ingridient;
 
   constructor( private slService: ShoppingListService) { }
 
   ngOnInit() {
-
+    this.subscription = this.slService.startedEditting.subscribe(
+        (index: number)=>{
+          this.editedItemIndex = index;
+          this.editMode = true;
+          this.editedItem = this.slService.getIngridient(index);
+          this.slForm.setValue({
+            name: this.editedItem.name,
+            amount: this.editedItem.amount
+          })
+        }
+      );
   }
 
-  onAddItem(event:Event){
-  	event.preventDefault();
-  	const ingName = this.nameInputRef.nativeElement.value;
-  	const ingAmount = this.amountInputRef.nativeElement.value;
-  	const newIngridient = new Ingridient(ingName, ingAmount);
-    this.slService.addIngridient(newIngridient);
+  onAddItem(form : NgForm){
+  	// event.preventDefault();
+    const value = form.value;
+  	const newIngridient = new Ingridient(value.name, value.amount);
+    if(this.editMode){
+      this.slService.updateIngridient(this.editedItemIndex, newIngridient);
+    }
+   else{
+     this.slService.addIngridient(newIngridient);
+   }
+   this.editMode = false;
+    form.reset()
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+
+  onClear(){
+    this.editMode = false;
+    this.slForm.reset();
+  }
+
+  onDelete(){
+    this.slService.deleteIngridient(this.editedItemIndex);
+    this.onClear();
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,7 +4,7 @@ import {Subject} from 'rxjs/Subject';
 export class ShoppingListService{
 
 	ingridientsChanged = new Subject<Ingridient[]>();
-
+	startedEditting = new Subject<number>();
 	private ingridients: Ingridient[]=[
 		new Ingridient('Apple', '4'),
 		new Ingridient('Tomatoe', '6')
@@ -14,9 +14,23 @@ export class ShoppingListService{
 		return this.ingridients.slice();
 	}
 
+	getIngridient(index: number){
+		return this.ingridients[index];
+	}
+
 	addIngridient(ingridient: Ingridient){
 		this.ingridients.push(ingridient);
 		this.ingridientsChanged.next( this.ingridients.slice() );
+	}
+
+	updateIngridient(index: number,  newIngridient: Ingridient){
+		this.ingridients[index] = newIngridient;
+		this.ingridientsChanged.next(this.ingridients.slice());
+	}
+
+	deleteIngridient(index: number){
+		this.ingridients.splice(index, 1);
+		this.ingridientsChanged.next(this.ingridients.slice());
 	}
 
 	addIngridients( ingridients: Ingridient[] ){
